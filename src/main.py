9ed06ceb4475bf6f0469fdf10151fad7d3ccfe66
Matt London <mrl2534@rit.edu>
2024-02-01 18:43:47 -0500

    Basic compile test

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from rnet_controller.RNetController import RNetController
+
+
+def main():
+    controller = RNetController(0)
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
