2d18d59af8414165effdaa0f5b2c2d535a09a5b8
Matt London <mrl2534@rit.edu>
2024-02-26 13:41:38 -0500

    Close the connection after attempting to open it in the main test

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from rnet_controller.RNetController import RNetController
 
 def main():
     controller = RNetController(0)
-    print("Hello World!")
+    controller.close()
 
 
 if __name__ == "__main__":
