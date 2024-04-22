8b740f20c89808022cb5f5d98697482a1d90dd99
Matt London <mrl2534@rit.edu>
2024-04-22 12:26:40 -0400

    Add pi side of the protocol

--- src/wheelchair_interface/clientserver/pi_server.py
+++ src/wheelchair_interface/clientserver/pi_server.py
@@ -1,41 +1,71 @@
 import serial
 import threading
+import logging
 import queue
+from ..rnet_controller.RNetController import RNetController
+from ..protocol.processor import decode_move_cmd, InvalidCmdException
 
 
 DEVICE = "/dev/ttyS0"
 BAUD_RATE = 115200
 
+RECONNECTION_ATTEMPTS = 5
 
-def process_command(command: bytearray):
+
+def process_command(controller: RNetController, command: bytearray) -> bool:
     """
     This will decide what needs to be run and send it to the correct function to send to the chair
 
+    :param controller: Interface to the wheelchair
     :param command: The command to be processed by the wheelchair
-    :return:
+    :return: If it could be processed and ran successfully
     """
-    print(f"Processing command: {command}")
+    try:
+        direction, duration = decode_move_cmd(command)
+        controller.drive_direction_seconds(direction, duration)
+        return True
+
+    except InvalidCmdException as e:
+        logging.error(e.message)
+        return False
 
 
-def process_commands(command_queue: queue.Queue):
+def process_commands(controller: RNetController, command_queue: queue.Queue):
     """
     Threaded function to pop commands and process them
 
+    :param controller: Interface to the wheelchair
     :param command_queue: The queue to get the commands from
     """
     while True:
         if not command_queue.empty():
             command = command_queue.get()
-            process_command(command)
+            process_command(controller, command)
             command_queue.task_done()
 
 
 def main():
+    # Establish an RNET controller interface
+    rnet_controller = None
+    for _ in range(RECONNECTION_ATTEMPTS):
+        logging.info("Attempting connection to wheelchair...")
+        rnet_controller = RNetController()
+        if rnet_controller.is_connected():
+            logging.info("Wheelchair connected successfully.")
+            break
+        else:
+            logging.error("Failed to connect to wheelchair")
+
+    # Ensure successful connection
+    if rnet_controller is None:
+        logging.error(f"Failed to connect after {RECONNECTION_ATTEMPTS}. Exiting..")
+        return
+
     # Create a queue to store commands
     command_queue = queue.Queue()
 
     # Start the thread for processing commands
-    thread = threading.Thread(target=process_commands, args=[command_queue])
+    thread = threading.Thread(target=process_commands, args=[rnet_controller, command_queue])
     thread.daemon = True
     thread.start()
 
@@ -45,11 +75,9 @@ def main():
             received_command = serial_device.readline().decode().strip()
 
             if received_command:
-                print(f"Received command: {received_command}")
+                logging.info(f"Received command: {received_command}")
                 # Add command to the queue
                 command_queue.put(received_command)
-
-    # TODO add break clause
 
 
 if __name__ == "__main__":
