d3ae3095e37f7f22f6af5c43bc4c7ae5d44a675f
Matt London <mrl2534@rit.edu>
2024-04-23 12:52:23 -0400

    Implement the nano side

--- src/wheelchair_interface/clientserver/nano_client.py
+++ src/wheelchair_interface/clientserver/nano_client.py
@@ -4,33 +4,62 @@ file: nano_client.py
 description: This program will take movement commands and send them over uart to the pi
 """
 import serial
+import socket
+import pickle
+import logging
 
-DEVICE = "/dev/ttyTHS1"
-BAUD_RATE = 115200
+from ..protocol.processor import encode_move_cmd
+from ..protocol.resources import *
 
 
-def send_command(command: list[int], serial_device: serial.Serial):
+def send_command(command: bytearray, serial_device: serial.Serial) -> None:
     """
     Sends a command to the chair
 
-    :param command: List of bytes to send
+    :param command: Bytes to send
     :param serial_device: Device to send the command to
     """
-    # TODO Make sure this works
-    serial_device.write(bytearray(command))
-    print(f"Sent command: {command}")
+    serial_device.write(command)
+    logging.info("Command sent successfully.")
+
+
+def process_data(data: bytes, serial_device: serial.Serial) -> None:
+    """
+    Process the data received over the socket
+
+    :param data: Bytes from the socket
+    :param serial_device: Serial device to send move command
+    """
+    direction, duration = pickle.loads(data)
+    logging.info(f"Received data: {data}")
+    cmd = encode_move_cmd(direction, duration)
+    logging.info(f"Sending command: {cmd}...")
+    send_command(cmd, serial_device)
 
 
 def main():
     """
     Main function responsible for sending the move commands to the wheelchair
     """
-    with serial.Serial(DEVICE, BAUD_RATE, timeout=1) as serial_device:
+    with serial.Serial(NANO_DEVICE, BAUD_RATE, timeout=1) as serial_device, \
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_device:
+        logging.info(f"Opening socket connection on: {SOCKET_HOST}:{SOCKET_PORT}")
+        socket_device.bind((SOCKET_HOST, SOCKET_PORT))
+        socket_device.listen(1)
+
         while True:
-            # TODO Placeholder
-            # Example moves forward for 4 seconds
-            command = [0xfe, 0x01, 0x04, 0x03, 0xff]
-            send_command(command, serial_device)
+            logging.info("Waiting for connection...")
+            conn, addr = socket_device.accept()
+            logging.info(f"Connection from {addr}.")
+
+            with conn:
+                while True:
+                    logging.info("Listening for command...")
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+
+                    process_data(data, serial_device)
 
 
 if __name__ == "__main__":
