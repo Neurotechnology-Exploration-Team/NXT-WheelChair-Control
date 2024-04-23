58f9672d73c598483f08c135abaeebb27906b8c4
Matt London <mrl2534@rit.edu>
2024-04-23 11:29:12 -0400

    Add super simple cli options for client/server

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
+import sys
+
 import wheelchair_interface.protocol
-import wheelchair_interface.clientserver
+from wheelchair_interface.clientserver import pi_server, nano_client
 import wheelchair_interface.rnet_controller
 
 
 def main():
-    print("Import success.")
+    if "server" in sys.argv:
+        pi_server.main()
+    elif "client" in sys.argv:
+        nano_client.main()
 
 
 if __name__ == "__main__":
